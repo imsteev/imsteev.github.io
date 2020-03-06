9a4ed754816772563aad1c2b9474e6c967c0b307
Stephen Chung <spchung95@gmail.com>
2020-03-05 20:02:22 -0800

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    resumeLink: 'https://docs.google.com/document/d/1UrTvP1jt-8MynZnAPaja6gaAjZKJK6sG89mi3JhN0iM/edit?usp=sharing',
+    currentCompany: 'Ultimate Software',
+    currentCompanyLink: 'https://www.ultimatesoftware.com/UltiPro-Solution-Features-Employee-Surveys'
+  }
+})
+
+console.log('hello')
