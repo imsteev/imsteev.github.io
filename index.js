3d8c11db51c63d2b5ffbcfd579307718d766edc8
Stephen Chung <spchung95@gmail.com>
2020-03-05 20:09:13 -0800

    cname

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!',
-    resumeLink: 'https://docs.google.com/document/d/1UrTvP1jt-8MynZnAPaja6gaAjZKJK6sG89mi3JhN0iM/edit?usp=sharing',
+    resumeLink: '',
     currentCompany: 'Ultimate Software',
     currentCompanyLink: 'https://www.ultimatesoftware.com/UltiPro-Solution-Features-Employee-Surveys'
   }
