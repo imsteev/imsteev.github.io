475c11319f62fc821f1ef59ed954b62948581b0f
Stephen Chung <spchung95@gmail.com>
2020-08-29 21:04:13 -0700

    animations

--- personal-website/src/PostContainer.js
+++ personal-website/src/PostContainer.js
@@ -1,15 +1,15 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { css, keyframes } from "styled-components";
 
 /**
  * A single post
  * Date and title are optional, but recommended
  */
-const Post = ({ children, date, title }) => {
+const Post = ({ children, date, title, enclosedTitle = false }) => {
   return (
     <StyledPost>
       <p className="header">
-        {date} {title && `"${title}"`}
+        {date} {title && enclosedTitle ? `"${title}"` : title}
       </p>
       {children}
     </StyledPost>
@@ -22,7 +22,10 @@ const Post = ({ children, date, title }) => {
  */
 const PostContainer = () => (
   <StyledPostContainer>
-    <Post date="August 29" title="v2">
+    <Post title="CSS Practice">
+      <CssPractice1 />
+    </Post>
+    <Post date="August 29" title="v2" enclosedTitle>
       <p>
         I previously implemented my personal website using Vue.js, but it
         quickly became hard to maintain without a proper component structure. So
@@ -34,7 +37,7 @@ const PostContainer = () => (
         often.
       </p>
     </Post>
-    <Post date="May 31" title="Unrest">
+    <Post date="May 31" title="Unrest" enclosedTitle>
       <p>
         Reflecting on my upbringing, I grew up in a sheltered, educated, and
         diverse neighborhood. For this reason, I don't feel entitled to the
@@ -97,6 +100,106 @@ const StyledPost = styled.div`
 const Afterthought = styled.p`
   font-size: 0.8rem;
   font-style: italic;
+`;
+
+const Pace = keyframes`
+  0% {
+    transform: translateX(0);
+  }
+
+  25% {
+    transform: translateX(2rem);
+  }
+
+  50% {
+    transform: translateX(4rem);
+  }
+  75% {
+    transform: translateX(2rem);
+  }
+  100% {
+    transform: translateX(0);
+  }
+`;
+
+const Rotate = keyframes`
+  to {
+    transform: rotate(359deg);
+  }
+`;
+
+const CssPractice1 = () => {
+  const [playing, setPlaying] = useState(false);
+  return (
+    <StyledCssPractice1 animate={playing}>
+      <div>
+        <button onClick={() => setPlaying(!playing)}>
+          {playing ? "pause animations" : "play animations"}
+        </button>
+        <Flex>
+          <>
+            <p>Look it's a rotating square</p>
+            <Animation className="box" id="animation-1" />
+          </>
+          <>
+            <p>This one's a mover</p>
+            <Animation className="box" id="animation-2" />
+          </>
+        </Flex>
+      </div>
+    </StyledCssPractice1>
+  );
+};
+
+const Animation = ({ animate, id, className }) => {
+  return <StyledAnimation animate={animate} className={className} id={id} />;
+};
+
+const StyledAnimation = styled.div``;
+const StyledCssPractice1 = styled.div`
+  .box {
+    width: 24px;
+    height: 24px;
+  }
+
+  #animation-1 {
+    background-color: blue;
+
+    ${({ animate }) =>
+      animate &&
+      css`
+        animation: ${Rotate} 4s linear infinite;
+      `}
+    margin: 1rem;
+    transition: background-color 0.4s;
+
+    :hover {
+      background-color: #fea423;
+    }
+  }
+
+  #animation-2 {
+    background-color: seagreen;
+    ${({ animate }) =>
+      animate &&
+      css`
+        animation: ${Pace} 1s linear infinite;
+      `}
+    margin: 1rem;
+  }
+`;
+
+const Button = styled.button`
+  max-height: 3rem;
+  appearance: none;
+  border: 0;
+  &:focus {
+    outline: none;
+  }
+`;
+const Flex = styled.div`
+  display: flex;
+  align-content: center;
 `;
 
 export default PostContainer;
